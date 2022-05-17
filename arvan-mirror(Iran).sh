c5f4fe633c801ad8d72defcc5c225c99a7e86f86
Hx1iCt <93550458+Hx1iCt@users.noreply.github.com>
2022-05-18 01:11:47 +0430

    Update arvan-mirror(Iran).sh

--- arvan-mirror(Iran).sh
+++ arvan-mirror(Iran).sh
@@ -1,7 +1,7 @@
 #!bin/sh
 
-mkdir /etc/pacman.d/bk
-mv /etc/pacman.d/mirrorlist /etc/pacman.d/bk
+mkdir /etc/pacman.d/bak
+mv /etc/pacman.d/mirrorlist /etc/pacman.d/bak
 touch /etc/pacman.d/mirrorlist
 echo "Server = https://mirror.arvancloud.com/archlinux/$repo/os/$arch" >> /etc/pacman.d/mirrorlist
 pacman -Syy

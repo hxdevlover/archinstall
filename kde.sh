20c4046ae9fa9c9ac7a2cd63462f608b4d8880f6
Hx <93550458+hxdevlover@users.noreply.github.com>
2022-05-18 06:38:17 +0430

    Update kde.sh

--- kde.sh
+++ kde.sh
@@ -3,7 +3,13 @@
 sudo timedatectl set-ntp true
 sudo hwclock --systohc
 
-pacman -Sy xorg sddm plasma firefox papirus-icon-theme dolphin-plugins kde-system-meta ark kate kcalc kfind konsole unzip p7zip gwenview okular archlinux-wallpaper dbus tilix plasma-wayland-session vlc openvpn
+pacman -Sy xorg sddm plasma firefox papirus-icon-theme dolphin-plugins kde-system-meta ark kate kcalc kfind konsole unzip p7zip gwenview okular archlinux-wallpaper dbus tilix plasma-session-wayland vlc openvpn
+
+# If you use Nvidia, Install these packages too:
+# pacman -S xf86-video-nouveau nvidia nvidia-settings
+
+# If you use Intel, Install these packages too:
+# pacman -S xf86-video-intel intel-ucode
 
 sudo systemctl enable dbus
 sudo systemctl enable sddm

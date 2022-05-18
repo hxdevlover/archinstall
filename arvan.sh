48e45669114dd0250a5845e98aa6b7e20dad9f14
Hx <93550458+hxdevlover@users.noreply.github.com>
2022-05-18 05:40:53 +0430

    Delete arvan.sh

--- arvan.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir /etc/pacman.d/bak
-mv /etc/pacman.d/mirrorlist /etc/pacman.d/bak
-touch /etc/pacman.d/mirrorlist
-echo "Server = https://mirror.arvancloud.com/archlinux/\$repo/os/\$arch" >> /etc/pacman.d/mirrorlist
-pacman -Syy

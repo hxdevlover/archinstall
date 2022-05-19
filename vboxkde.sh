27be6999a737c90e34fff661b920c5038eadbc66
Hx <93550458+hxdevlover@users.noreply.github.com>
2022-05-20 00:15:42 +0430

    Update vboxkde.sh

--- vboxkde.sh
+++ vboxkde.sh
@@ -5,12 +5,6 @@ sudo hwclock --systohc
 
 pacman -Sy xf86-video-vmware xorg sddm plasma firefox papirus-icon-theme dolphin-plugins kde-system-meta ark kate kcalc kfind konsole unzip p7zip gwenview okular archlinux-wallpaper dbus plasma-wayland-session vlc openvpn
 
-# If you use Nvidia, Install these packages too:
-# pacman -S xf86-video-nouveau nvidia nvidia-settings
-
-# If you use Intel, Install these packages too:
-# pacman -S xf86-video-intel intel-ucode
-
 sudo systemctl enable dbus
 sudo systemctl enable sddm
 

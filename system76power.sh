632c21cacfce3a81f879ea34c15bc2981d7f8876
Hx <93550458+hxdevlover@users.noreply.github.com>
2022-05-20 00:20:18 +0430

    Update system76power.sh

--- system76power.sh
+++ system76power.sh
@@ -2,6 +2,8 @@
 
 pikaur -Sy system76-power
 sudo systemctl enable --now system76-power
-sudo system76-power graphics integrated
+# sudo system76-power graphics integrated
+# sudo system76-power graphics nvidia
+
 pikaur -S auto-cpufreq
 sudo systemctl enable --now auto-cpufreq

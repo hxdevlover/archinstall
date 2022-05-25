32da35374d9f794580a76b20657a998a30958e8f
Hx <93550458+hxdevlover@users.noreply.github.com>
2022-05-25 17:12:18 +0430

    Update yay.sh

--- yay.sh
+++ yay.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-sudo pacman -Syy
-sudo pacman -S --noconfirm --needed base-devel git
+sudo pacman -Sy --noconfirm --needed base-devel git
 
 sudo git clone https://aur.archlinux.org/yay.git
 sudo chown -R  hx:hx yay

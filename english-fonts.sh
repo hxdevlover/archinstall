c24adc9190643ee689727407aa76989f8b3be333
Hx <93550458+hxdevlover@users.noreply.github.com>
2022-05-28 11:12:19 +0430

    Add files via upload

---
+++ english-fonts.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+#install English Fonts
+sudo pacman -Sy --noconfirm --needed otf-font-awesome ttf-hack ttf-dejavu gnu-free-fonts noto-fonts ttf-roboto ttf-ubuntu-font-family ttf-anonymous-pro ttf-fira-mono ttf-fira-code adobe-source-code-pro-fonts adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk noto-fonts-emoji
+
+yay -S ttf-mononoki

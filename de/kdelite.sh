80002e57f2ff5630144f724e62f993631bcf9117
Hx <93550458+hxdevlover@users.noreply.github.com>
2022-05-29 13:28:25 +0430

    Add files via upload

---
+++ de/kdelite.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+sudo timedatectl set-ntp true
+sudo hwclock --systohc
+
+pacman -Sy --noconfirm --needed xorg sddm plasma pcmanfm firefox papirus-icon-theme materia-kde materia-gtk-theme ksystemlog gparted ark kate kcalc kfind kcron unzip p7zip gwenview okular archlinux-wallpaper dbus vlc openvpn spectacle kwallet kwalletmanager alacritty fish --ignore discover, oxygen
+
+# If you use Nvidia, Install these packages too:
+# pacman -S xf86-video-nouveau nvidia nvidia-utils nvidia-settings plasma-wayland-session 
+
+# If you use Intel, Install these packages too:
+# pacman -S xf86-video-intel intel-ucode
+
+# If you use AMD, Install these packages too:
+# pacman -S xf86-video-amdgpu
+
+# Install more icons (Install yay first -- Run yay.sh script):
+# yay -Sy fluent-icon-theme-git
+
+sudo systemctl enable dbus
+sudo systemctl enable sddm
+
+reboot

07cd1e1fa560ffb24c029ccd8e4553735e63f2f6
Hx <93550458+hxdevlover@users.noreply.github.com>
2022-05-29 18:03:17 +0430

    Add files via upload

---
+++ kde-xmonad/kde-xmonad.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+sudo pacman -Sy --noconfirm --needed xorg sddm plasma xmonad xmonad-contrib picom pcmanfm firefox papirus-icon-theme materia-kde materia-gtk-theme ksystemlog gparted ark kate kcalc kfind kcron unzip p7zip gwenview okular archlinux-wallpaper dbus vlc openvpn spectacle kwallet kwalletmanager alacritty fish
+
+# If you use Nvidia, Install these packages too:
+# pacman -S xf86-video-nouveau nvidia nvidia-utils nvidia-settings
+
+# If you use Intel, Install these packages too:
+# pacman -S xf86-video-intel intel-ucode
+
+# If you use AMD, Install these packages too:
+# pacman -S xf86-video-amdgpu
+
+sudo systemctl enable dbus
+sudo systemctl enable sddm

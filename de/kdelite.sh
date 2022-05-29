7480de1bd844cff94876e6dc5f43fe86103b1d7c
Hx <93550458+hxdevlover@users.noreply.github.com>
2022-05-29 17:52:35 +0430

    Update kdelite.sh

--- de/kdelite.sh
+++ de/kdelite.sh
@@ -3,7 +3,7 @@
 sudo timedatectl set-ntp true
 sudo hwclock --systohc
 
-pacman -Sy --noconfirm --needed xorg sddm plasma pcmanfm firefox papirus-icon-theme materia-kde materia-gtk-theme ksystemlog gparted ark kate kcalc kfind kcron unzip p7zip gwenview okular archlinux-wallpaper dbus vlc openvpn spectacle kwallet kwalletmanager alacritty fish --ignore discover, oxygen
+pacman -Sy --noconfirm --needed xorg sddm plasma pcmanfm firefox papirus-icon-theme materia-kde materia-gtk-theme ksystemlog gparted ark kate kcalc kfind kcron unzip p7zip gwenview okular archlinux-wallpaper dbus vlc openvpn spectacle kwallet kwalletmanager alacritty fish
 
 # If you use Nvidia, Install these packages too:
 # pacman -S xf86-video-nouveau nvidia nvidia-utils nvidia-settings plasma-wayland-session 

af726e72841b56d85753a28dcbf47c11c64f7b5d
Hx <93550458+hxdevlover@users.noreply.github.com>
2022-05-29 13:21:25 +0430

    Update xmonadonxfce.sh

--- wm/xmonad/xfce4/xmonadonxfce.sh
+++ wm/xmonad/xfce4/xmonadonxfce.sh
@@ -1,5 +1,12 @@
 #! /bin/bash
 
 sudo pacman -Sy --noconfirm --needed xorg xmonad xmonad-contrib lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings networkmanager network-manager-applet pulseaudio alacritty dmenu htop fish lxappearance xterm nitrogen
+sudo pacman -R xfdesktop
+
+sudo touch .xprofile
+sudo tee -a .xprofile <<EOF
+nitrogen --restore &
+xmonad --replace &
+
 
 sudo systemctl enable lightdm

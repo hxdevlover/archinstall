161e03e79e25cbeb819de46e0b58620d49b1ef9d
Hx <93550458+hxdevlover@users.noreply.github.com>
2022-05-29 13:45:43 +0430

    Add files via upload

---
+++ de/xmonadOnxfce4.sh
@@ -0,0 +1,31 @@
+#! bin/bash
+
+sudo pacman -S --noconfirm --needed xorg lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings xfce4 xfce4-goodies firefox papirus-icon-theme materia-gtk-theme vlc unzip p7zip pcmanfm archlinux-wallpaper alacritty fish xterm --ignore thunar, thunar-volman, xfdesktop, xfce4-terminal
+
+sudo touch $HOME/.xprofile
+sudo tee -a $HOME/.xprofile <<EOF
+nitrogen --restore &
+xmonad --replace &
+pulseaudio --start &
+EOF
+
+sudo mkdir $HOME/.xmonad
+sudo cp -r $HOME/archinstall/wm/xmonad/xfce4/xmonad.hs $HOME/.xmonad
+
+sudo mkdir $HOME/.config/alacritty
+sudo cp -r $HOME/archinstall/alacritty.yml $HOME/.config/alacritty
+
+xmonad --recompile
+
+# If you use Nvidia, Install these packages too:
+# pacman -S xf86-video-nouveau nvidia nvidia-utils nvidia-settings
+
+# If you use Intel, Install these packages too:
+# pacman -S xf86-video-intel intel-ucode
+
+# If you use AMD, Install these packages too:
+# pacman -S xf86-video-amdgpu
+
+sudo systemctl enable lightdm
+
+

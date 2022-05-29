fe9d287f7505aef896d30830286c6d67d823dc69
Hx <93550458+hxdevlover@users.noreply.github.com>
2022-05-29 18:05:50 +0430

    Update kde-xmonad.sh

--- kde-xmonad/kde-xmonad.sh
+++ kde-xmonad/kde-xmonad.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-sudo pacman -Sy --noconfirm --needed xorg sddm plasma xmonad xmonad-contrib picom pcmanfm firefox papirus-icon-theme materia-kde materia-gtk-theme ksystemlog gparted ark kate kcalc kfind kcron unzip p7zip gwenview okular archlinux-wallpaper dbus vlc openvpn spectacle kwallet kwalletmanager alacritty fish
+sudo pacman -Sy --noconfirm --needed xorg sddm plasma xmonad xmonad-contrib picom pcmanfm firefox papirus-icon-theme materia-kde materia-gtk-theme ksystemlog gparted ark kate kcalc kfind kcron unzip p7zip gwenview okular archlinux-wallpaper dbus vlc openvpn spectacle kwallet kwalletmanager alacritty fish xfce4-appfinder
 
 # If you use Nvidia, Install these packages too:
 # pacman -S xf86-video-nouveau nvidia nvidia-utils nvidia-settings

646ab1b134c17b004c9a3d9e7727f1ddf907fe75
Hx <93550458+hxdevlover@users.noreply.github.com>
2022-05-30 17:30:33 +0430

    Update xmonad-install.sh

--- wm/xmonad/xmonad-install.sh
+++ wm/xmonad/xmonad-install.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 #install Packages
-sudo pacman -Sy --noconfirm --needed xorg xmonad xmonad-contrib xmobar xorg lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings firefox vim nano networkmanager network-manager-applet pulseaudio volumeicon arc-gtk-theme arc-icon-theme papirus-icon-theme nitrogen picom alacritty arandr xorg-xrandr dmenu pcmanfm conky trayer htop fish lxappearance xterm
+sudo pacman -Sy --noconfirm --needed xorg xmonad xmonad-contrib xmobar lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings firefox vim nano networkmanager network-manager-applet pulseaudio volumeicon arc-gtk-theme arc-icon-theme papirus-icon-theme nitrogen picom alacritty arandr xorg-xrandr dmenu pcmanfm conky trayer htop fish lxappearance xterm
 
 #Make lightdm start at startup
 sudo systemctl enable lightdm

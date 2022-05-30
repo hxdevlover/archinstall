e669d16f03417ddd7ff792aded60ab9f6b717aff
Hx <93550458+hxdevlover@users.noreply.github.com>
2022-05-30 17:31:19 +0430

    Update xmonadonxfce.sh

--- wm/xmonad/xfce4/xmonadonxfce.sh
+++ wm/xmonad/xfce4/xmonadonxfce.sh
@@ -1,6 +1,4 @@
 #! /bin/bash
 
-sudo pacman -Sy --noconfirm --needed xorg xmonad xmonad-contrib alacritty htop fish nitrogen xfce4-appfinder xfce4-whiskermenu-plugin
+sudo pacman -Sy --noconfirm --needed xorg xmonad xmonad-contrib alacritty htop fish nitrogen picom xfce4-appfinder xfce4-whiskermenu-plugin
 sudo pacman -R xfdesktop
-
-sudo systemctl enable lightdm

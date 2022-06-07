90dd32654aca37d84bbbd5edef5bdff513be89d9
Hx <93550458+hxdevlover@users.noreply.github.com>
2022-06-07 13:39:51 +0430

    Update kdelite.sh

--- de/kdelite.sh
+++ de/kdelite.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
 
-sudo timedatectl set-ntp true
-sudo hwclock --systohc
-
-pacman -Sy --noconfirm --needed xorg sddm plasma thunar thunar-volman firefox papirus-icon-theme materia-kde materia-gtk-theme ksystemlog gparted ark kate kcalc kfind kcron unzip p7zip gwenview okular archlinux-wallpaper dbus vlc openvpn spectacle kwallet kwalletmanager alacritty fish
+sudo pacman -Sy --noconfirm --needed xorg sddm plasma pcmanfm-qt firefox papirus-icon-theme materia-kde ksystemlog gparted ark kate kcalc krunner kfind kcron unrar unzip p7zip gwenview okular archlinux-wallpaper dbus vlc openvpn spectacle kwallet kwalletmanager alacritty fish emacs
 
 # If you use Nvidia, Install these packages too:
-# pacman -S xf86-video-nouveau nvidia nvidia-utils nvidia-settings plasma-wayland-session 
+# sudo pacman -S xf86-video-nouveau nvidia nvidia-utils nvidia-settings plasma-wayland-session 
 
 # If you use Intel, Install these packages too:
-# pacman -S xf86-video-intel intel-ucode
+# sudo pacman -S xf86-video-intel intel-ucode
 
 # If you use AMD, Install these packages too:
-# pacman -S xf86-video-amdgpu
+# sudo pacman -S xf86-video-amdgpu
 
 # Install more icons (Install yay first -- Run yay.sh script):
 # yay -Sy fluent-icon-theme-git
 
 sudo systemctl enable dbus
 sudo systemctl enable sddm
-
-reboot

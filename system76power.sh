40ed057da74bcd6033ccc5070cad9385941dc31e
Hx <93550458+hxdevlover@users.noreply.github.com>
2022-06-23 12:00:58 +0430

    Delete system76power.sh

--- system76power.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-pikaur -Sy system76-power
-sudo systemctl enable --now system76-power
-# sudo system76-power graphics integrated
-# sudo system76-power graphics nvidia
-
-pikaur -S auto-cpufreq
-sudo systemctl enable --now auto-cpufreq

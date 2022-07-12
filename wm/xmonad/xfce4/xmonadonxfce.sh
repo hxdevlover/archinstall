f3ff3bbcea0e2182e903610d8326913579551b00
Hx <93550458+hxdevlover@users.noreply.github.com>
2022-07-12 21:17:39 +0430

    Delete wm/xmonad directory

--- wm/xmonad/xfce4/xmonadonxfce.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-sudo pacman -Sy --noconfirm --needed xmonad xmonad-contrib alacritty htop fish nitrogen picom xfce4-appfinder
-sudo pacman -R --noconfirm xfdesktop
--- wm/xmonad/xmonad-install.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-#install Packages
-sudo pacman -Sy --noconfirm --needed xorg xmonad xmonad-contrib xmobar lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings firefox vim nano networkmanager network-manager-applet pulseaudio volumeicon arc-gtk-theme arc-icon-theme papirus-icon-theme nitrogen picom alacritty arandr xorg-xrandr dmenu pcmanfm conky trayer htop fish lxappearance xterm
-
-#Make lightdm start at startup
-sudo systemctl enable lightdm
-
